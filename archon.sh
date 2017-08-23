fc2b6590dba6c5954691ec93d2fdf49ab7c3e317
billniakas <billniakas@gmail.com>
2017-08-23 12:04:36 +0300

    fixes...

--- archon.sh
+++ archon.sh
@@ -3,57 +3,74 @@ clear
 setfont gr928-8x16-thin.psfu
 echo '--------------- Archon ----------------'
 echo 'ο πρώτος Ελληνικός Arch Linux Installer'
-echo
-sleep 1
-echo 
+echo '---------------------------------------'
+sleep 2
+echo '---------------------------------------'
 echo 'Έλεγχος σύνδεσης στο διαδίκτυο'
-echo 
+echo '---------------------------------------'
 ping -c 5 www.google.com
 if [ $? -eq 0 ]; then
+  echo '---------------------------------------'
   echo 'Υπάρχει σύνδεση στο διαδίκτυο'
   echo 'Η εγκατάσταση μπορεί να συνεχιστεί'
+  echo '---------------------------------------'
 else
   echo 'Ελέξτε αν υπάρχει σύνδεση στο διαδίκτυο'
   exit	
 fi
 
-echo 
+echo '--------------------------------------------'
 echo 'Διαλέξτε το δίσκο που θα γίνει η εγκατάσταση'
-echo 
+echo '--------------------------------------------'
 lsblk | grep -i sd
+echo '--------------------------------------------------------'
 read -p "Σε ποιο δίσκο (/dev/sd?) θα εγκατασταθεί το Arch;" diskvar
+echo '--------------------------------------------------------'
 echo
+echo '--------------------------------------------------------'
 echo "Η εγκατάσταση θα γίνει στον" $diskvar
+echo '--------------------------------------------------------'
 echo
+sleep 2
 echo 
 echo 'Δημιουργία κατάτμησης'
 echo  
-SIZE=$(parted $diskvar print unit MB print free | grep -i "Disk /dev/sd " | head -n 1 | grep -oE "(\w+MB")
+SIZE=$(parted $diskvar print unit MB print free | grep -i "Disk /dev/sd " | head -n 1 | grep -oE "(\w+MB)")
 parted $diskvar mklabel gpt
 parted $diskvar print
 parted --align optimal $diskvar mkpart primary ext4 0% $SIZE
 parted $diskvar print
 mkfs.ext4 $diskvar
 echo 
+echo '--------------------------------------------------------'
 echo 'Προσάρτηση των Partition του Arch Linux'
+echo '--------------------------------------------------------'
 echo 
+sleep 1
 mount $diskvar /mnt
 echo 
+echo '--------------------------------------------------------'
 echo 'Προσθήκη πηγών λογισμικού (Mirrors)'
-echo 
+echo '--------------------------------------------------------'
+echo
+sleep 1 
 pacman -Syy
 pacman -S reflector
 reflector --latest 10 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
 echo 
+echo '--------------------------------------------------------'
 echo 'Εγκατάσταση της Βάσης του Arch Linux'
 echo 'Αν δεν έχετε κάνει ακόμα καφέ…. τώρα είναι η ευκαιρία...'
+echo '--------------------------------------------------------'
 echo
 sleep 2
-pacstrap -i /mnt base base-devel
+pacstrap -i /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
 echo 
+echo '--------------------------------------------------------'
 echo 'Είσοδος στο εγκατεστημένο Arch Linux'
+echo '--------------------------------------------------------'
 echo 
 arch-chroot /mnt
 echo 
